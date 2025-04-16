4c46a1dca577595fd7e299e81157e7911028a0ee
Brandon Gonzalez <bgbeaconhous@gmail.com>
2025-04-16 12:37:44 -0700

    created log in and register api and token and bcrypt

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,5 +1,10 @@
+require("dotenv").config()
+
 const express = require("express")
 const app = express();
+
+const prisma = require("./prisma");
+
 const cors = require("cors");
 const PORT = 3000
 
@@ -8,12 +13,42 @@ app.use(require("morgan")("dev"));
 
 app.use(cors({ origin: "http://localhost:5173" }));
 
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const verifyToken = require("./verify")
+
 app.use("/api", require("./api"));
 
+app.post("/api/register", async (req, res, next) => {
+    const {email, password} = req.body
+    const newUser = await prisma.user.create({
+      data: {email, password: await bcrypt.hash(password, 5)}
+    })
+    const token = jwt.sign({id: newUser.id, email: newUser.email}, process.env.JWT_SECRET)
+  
+    res.status(201).json(token)
+  })
 
 
 
+app.post("/api/login", async (req, res, next) => {
+    const {email, password} = req.body
+    const user = await prisma.user.findUnique({
+      where: {email}
+    })
+    if (!user) return res.status(400).json("User not found.")
+      console.log(user);
+      
+    const passwordMatch = await bcrypt.compare(password, user.password)
+  
+    if (!passwordMatch) return res.status(401).json("Account not found")
+      const token = jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET)
+  
+    res.status(201).json(token)
+  })
 
+
+  
 
 app.use((err, req, res, next) => {
     console.error(err);
